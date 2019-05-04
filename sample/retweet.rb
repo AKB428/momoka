9eb252a269b2a85b1a03c63b6558ed81dd264745
AKB428 <gandara016+1@gmail.com>
2019-05-05 00:36:22 +0900

    fix retweet

--- sample/retweet.rb
+++ sample/retweet.rb
@@ -2,10 +2,26 @@ require 'pp'
 require "./twitter.rb"
 
 #https://obel.hatenablog.jp/entry/20170629/1498707124
+#https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/get-statuses-retweeters-ids.html
+#https://www.rubydoc.info/gems/twitter/Twitter/REST/Tweets#retweets-instance_method
 #
 query = ARGV[0]
 
-rt_tweets = @tw.retweets(query, {"count": 100})
+cursor = 1
+result = {}
+
+# cursorオプションはRuby gem経由で設定されない、そもそもAPI側でも無効っぽい
+rt_tweets = @tw.retweets(query, {"count": 100, "cursor": cursor})
+
 rt_tweets.each do |rt_tweet|
-  puts "screen_name: #{rt_tweet.id}, screen_name: #{rt_tweet.user.screen_name}"
+  #puts "#{rt_tweet.id},#{rt_tweet.user.screen_name},https://twitter.com/#{rt_tweet.user.screen_name}/status/#{rt_tweet.id}"
+    result[rt_tweet.id] = rt_tweet.user.screen_name
+end
+
+result.sort.each do |r|
+
+  id = r[0]
+  twitter_name = r[1]
+
+  puts "#{id},#{twitter_name},https://twitter.com/#{twitter_name}/status/#{id}"
 end
